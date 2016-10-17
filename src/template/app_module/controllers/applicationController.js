a3c11b1c3ce714b3a0a724404d304a09856b7c63
anhgien <giencntt@gmal.com>
2016-10-18 00:28:03 +0700

    fix dropdown menu

--- src/template/app_module/controllers/applicationController.js
+++ src/template/app_module/controllers/applicationController.js
@@ -7,72 +7,68 @@
  * this is the main controller for all app and it is always present
  */
 define(['app', 'servicesModule', 'directives/scroll-top/scroll-top.js'], function(app) {
+  
+  function appController($rootScope, 
+        $scope, AUTH_EVENTS, Session, $state, USER_ROLES,
+        ngProgressFactory, myRouter, AuthService, utils) {
+    
+    
+    var ngProgress = ngProgressFactory.createInstance();
+    $scope.logout = function() {
+      AuthService.logout()
+        .then(function(res) {
+            console.log("logout", res);
+            if (utils.isSuccess(res.data.success)) {
+                Session.delete();
+                myRouter.init();
+                $state.go('app.login');
+            }
+        });
+    }
 
-    app.controller('applicationController',
-        ['$rootScope',
-            '$scope',
-            'AUTH_EVENTS',
-            'Session',
-            '$state',
-            'ngProgressFactory',
-            'myRouter',
-            'AuthService',
-            'utils',
-            function($rootScope, 
-            $scope, AUTH_EVENTS, Session, $state, 
-            ngProgressFactory, myRouter, AuthService, utils) {
-                //$log.info('APPLICATION CTRL');
-                var ngProgress = ngProgressFactory.createInstance();
-                
-                $scope.setCurrentUser = function(user) {
+    $scope.$on('LoadDone', function(event, success) {
+      if (success) {
+          ngProgress.complete();
+      } else {
+          ngProgress.stop();
+      }
+    });
 
-                };
-                
-                $scope.logout = function() {
-                    AuthService.logout()
-                            .then(function(res) {
-                                console.log("logout", res);
-                                if (utils.isSuccess(res.data.success)) {
-                                    Session.delete();
-                                    myRouter.init();
-                                    $state.go('app.login');
-                                }
-                            });
-                }
-                
-                $scope.$on('LoadDone', function(event, success) {
-                    console.log('broadcast LoadDone');
-                    if (success) {
-                        ngProgress.complete();
-                    } else {
-                        ngProgress.stop();
-                    }
-                });
-                
-                $scope.$on('LoadStart', function(event) {
-                    console.log('broadcast LoadStart');
-                    ngProgress.start();
-                });
-                
-                $scope.$on(AUTH_EVENTS.notAuthenticated, function(e, event) {
-                    event.preventDefault();
-                    console.log('broadcast');
-                    Session.delete();
-                    $state.go('app.login');
-                    myRouter.init();
-                });
+    $scope.$on('LoadStart', function(event) {
+      ngProgress.start();
+    });
+
+    $scope.$on(AUTH_EVENTS.notAuthenticated, function(e, event) {
+      event.preventDefault();
+      Session.delete();
+      $state.go('app.login');
+      myRouter.init();
+    });
 
 
-                // bind global keypress event
-                $(document).on('keydown', function(e) {
-                    $rootScope.$broadcast('globalKeyDown', e.keyCode);
-                });
+    // bind global keypress event
+    $(document).on('keydown', function(e) {
+      $rootScope.$broadcast('globalKeyDown', e.keyCode);
+    });
 
-                $(window).mousedown(function(e) {
-                    $rootScope.$broadcast('globalMouseDown', e, this);
-                });
-
-
-            }]);
+    $(window).mousedown(function(e) {
+      $rootScope.$broadcast('globalMouseDown', e, this);
+    });
+  }
+      
+  appController.$inject = [
+    '$rootScope',
+    '$scope',
+    'AUTH_EVENTS',
+    'Session',
+    '$state',
+    'USER_ROLES',
+    'ngProgressFactory',
+    'myRouter',
+    'AuthService',
+    'utils'
+  ];
+  
+  app.controller('applicationController', appController);
 
 });
--- src/template/app_module/controllers/homeController.js
+++ src/template/app_module/controllers/homeController.js
@@ -4,11 +4,47 @@ define([
   ],
   function(app) {
     
-    function homeController(vm, Session) {
+    function homeController(vm, Session, USER_ROLES) {
       console.log("---HomeController--->")
       vm.userRole = Session.getUserRole();
+      
+      var dropdownMenu = [];
+      console.log("---userRole-->", Session.getUserRole());
+      switch (Session.getUserRole()) {
+        case USER_ROLES.student: 
+          dropdownMenu = [
+            {url: "#/dashboard/job", icon: "fa fa-briefcase", title: "Job"},
+            {url: "#/dashboard/preference", icon: "fa fa-heart", title: "Preferences"},
+            {url: "#/dashboard/profile", icon: "fa fa-pencil-square", title: "Profile"},
+            {url: "#/dashboard/inbox", icon: "fa fa-envelope-o", title: "Inbox"},
+            {url: "#/dashboard/files", icon: "fa fa-file", title: "Files"}
+          ];
+          break;
+
+        case USER_ROLES.agency:
+          dropdownMenu = [
+            {url: "#/dashboard/job", icon: "fa fa-briefcase", title: "Job"},
+            {url: "#/dashboard/profile", icon: "fa fa-pencil-square", title: "Profile"},
+            {url: "#/dashboard/inbox", icon: "fa fa-envelope-o", title: "Inbox"},
+            {url: "#/dashboard/files", icon: "fa fa-file", title: "Files"}
+          ];
+          break;
+
+        case USER_ROLES.admin:
+          dropdownMenu = [
+            {url: "#/dashboard/job", icon: "fa fa-briefcase", title: "Job"},
+            {url: "#/dashboard/agencies", icon: "fa fa-pencil-square", title: "Profile"},
+            {url: "#/dashboard/inbox", icon: "fa fa-envelope-o", title: "Inbox"},
+          ];
+          break;
+
+        default:
+          break;
+      }
+      
+      vm.dropdownMenu = dropdownMenu;
     }
     
-    homeController.$inject = ["$scope", "Session"];
+    homeController.$inject = ["$scope", "Session", "USER_ROLES"];
     app.controller("homeController", homeController);
 });
